6328c67df6d6dca487e98ecf8820715fecf230d7
Djumabaevs <djumabaevb@gmail.com>
2021-06-23 13:17:13 +0600

    on back pressed dispatcher

---
+++ app/src/main/java/com/bignerdranch/android/artbooktesting/view/ArtFragment.kt
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.artbooktesting.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bignerdranch.android.artbooktesting.R
+import com.bignerdranch.android.artbooktesting.databinding.FragmentArtsBinding
+
+class ArtFragment : Fragment(R.layout.fragment_arts) {
+
+    private var fragmentBinding: FragmentArtsBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtsBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
+        }
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
+    }
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/artbooktesting/view/ImageApiFragment.kt
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.artbooktesting.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.bignerdranch.android.artbooktesting.R
+
+class ImageApiFragment : Fragment(R.layout.fragment_image_api) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
