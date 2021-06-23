fc8c3daa75b0cc513323c962ee54e779c96f71c6
Djumabaevs <djumabaevb@gmail.com>
2021-06-23 12:23:57 +0600

    layouts created xml

--- app/src/main/java/com/bignerdranch/android/artbooktesting/view/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/artbooktesting/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bignerdranch.android.artbooktesting
+package com.bignerdranch.android.artbooktesting.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.artbooktesting.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
