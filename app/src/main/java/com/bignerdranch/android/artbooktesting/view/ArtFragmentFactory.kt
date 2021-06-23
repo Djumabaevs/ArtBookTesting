e5a8d7f072c934002048c004a952d775cd58e536
Djumabaevs <djumabaevb@gmail.com>
2021-06-24 00:49:35 +0600

    add recycler adapter into factory of fragments

--- app/src/main/java/com/bignerdranch/android/artbooktesting/view/ArtFragmentFactory.kt
+++ app/src/main/java/com/bignerdranch/android/artbooktesting/view/ArtFragmentFactory.kt
@@ -2,16 +2,19 @@ package com.bignerdranch.android.artbooktesting.view
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.bignerdranch.android.artbooktesting.adapter.ArtRecyclerAdapter
 import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
 class ArtFragmentFactory @Inject constructor(
+    private val artRecyclerAdapter: ArtRecyclerAdapter,
     private val glide: RequestManager
 ) : FragmentFactory() {
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
 
         return when(className) {
+            ArtFragment::class.java.name -> ArtFragment(artRecyclerAdapter)
             ArtDetailsFragment::class.java.name -> ArtDetailsFragment(glide)
             else -> super.instantiate(classLoader, className)
         }
