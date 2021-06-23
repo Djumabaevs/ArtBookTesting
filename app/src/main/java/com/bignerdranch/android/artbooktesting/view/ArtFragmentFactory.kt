4849bae5cca3e7b1df43ac259c06620f396dbf8e
Djumabaevs <djumabaevb@gmail.com>
2021-06-24 00:59:16 +0600

    add same recycler injection to image fragment

--- app/src/main/java/com/bignerdranch/android/artbooktesting/view/ArtFragmentFactory.kt
+++ app/src/main/java/com/bignerdranch/android/artbooktesting/view/ArtFragmentFactory.kt
@@ -3,11 +3,13 @@ package com.bignerdranch.android.artbooktesting.view
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bignerdranch.android.artbooktesting.adapter.ArtRecyclerAdapter
+import com.bignerdranch.android.artbooktesting.adapter.ImageRecyclerAdapter
 import com.bumptech.glide.RequestManager
 import javax.inject.Inject
 
 class ArtFragmentFactory @Inject constructor(
     private val artRecyclerAdapter: ArtRecyclerAdapter,
+    private val imageRecyclerAdapter: ImageRecyclerAdapter,
     private val glide: RequestManager
 ) : FragmentFactory() {
 
@@ -15,6 +17,7 @@ class ArtFragmentFactory @Inject constructor(
 
         return when(className) {
             ArtFragment::class.java.name -> ArtFragment(artRecyclerAdapter)
+            ImageApiFragment::class.java.name -> ImageApiFragment(imageRecyclerAdapter)
             ArtDetailsFragment::class.java.name -> ArtDetailsFragment(glide)
             else -> super.instantiate(classLoader, className)
         }
