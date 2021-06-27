5f318867c4e4cc5f73e5c12207e9b8e085735ae7
Djumabaevs <djumabaevb@gmail.com>
2021-06-27 12:02:51 +0600

    image api fragment's binding and viewmodel

--- app/src/main/java/com/bignerdranch/android/artbooktesting/view/ImageApiFragment.kt
+++ app/src/main/java/com/bignerdranch/android/artbooktesting/view/ImageApiFragment.kt
@@ -3,16 +3,34 @@ package com.bignerdranch.android.artbooktesting.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.bignerdranch.android.artbooktesting.R
 import com.bignerdranch.android.artbooktesting.adapter.ImageRecyclerAdapter
+import com.bignerdranch.android.artbooktesting.databinding.FragmentImageApiBinding
+import com.bignerdranch.android.artbooktesting.viewmodel.ArtViewModel
 import javax.inject.Inject
 
 class ImageApiFragment @Inject constructor(
     private val imageRecyclerAdapter: ImageRecyclerAdapter
 ) : Fragment(R.layout.fragment_image_api) {
 
+    lateinit var viewModel: ArtViewModel
+
+    private var fragmentBinding: FragmentImageApiBinding? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+
+        val binding = FragmentImageApiBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.imageRecyclerView.adapter = imageRecyclerAdapter
+        binding.imageRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+
+
 
 
     }
