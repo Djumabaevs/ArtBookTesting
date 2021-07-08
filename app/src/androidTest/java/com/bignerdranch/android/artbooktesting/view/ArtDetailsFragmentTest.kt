e7a785d47ff59b245a3f90da15b9cd80addbeeaf
Djumabaevs <djumabaevb@gmail.com>
2021-07-08 10:47:13 +0600

    test on back pressed

--- app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ArtDetailsFragmentTest.kt
+++ app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ArtDetailsFragmentTest.kt
@@ -4,6 +4,7 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.MediumTest
@@ -15,6 +16,8 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import androidx.test.espresso.Espresso.pressBack
 import javax.inject.Inject
 
 @MediumTest
@@ -44,11 +47,23 @@ class ArtDetailsFragmentTest {
             Navigation.setViewNavController(requireView(), navController)
         }
 
-        Espresso.onView(ViewMatchers.withId(R.id.artImageView)).perform(click())
+        Espresso.onView(ViewMatchers.withId(R.id.artImageView)).perform(ViewActions.click())
 
         Mockito.verify(navController).navigate(
             ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment()
         )
     }
 
+    @Test
+    fun testOnBackPressed() {
+        val navController = Mockito.mock(NavController::class.java)
+        launchFragmentInHiltContainer<ArtDetailsFragment>(
+            factory = fragmentFactory
+        ) {
+            Navigation.setViewNavController(requireView(),navController)
+        }
+
+        pressBack()
+        verify(navController).popBackStack()
+    }
 }
