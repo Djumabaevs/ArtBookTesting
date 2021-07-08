6e880b160f14d0db2ae7b2875ff3b057c87e95f5
Djumabaevs <djumabaevb@gmail.com>
2021-07-08 12:59:23 +0600

    test select image

--- app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ImageApiFragmentTest.kt
+++ app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ImageApiFragmentTest.kt
@@ -1,9 +1,24 @@
 package com.bignerdranch.android.artbooktesting.view
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.bignerdranch.android.artbooktesting.R
+import com.bignerdranch.android.artbooktesting.adapter.ImageRecyclerAdapter
+import com.bignerdranch.android.artbooktesting.getOrAwaitValue
+import com.bignerdranch.android.artbooktesting.launchFragmentInHiltContainer
+import com.bignerdranch.android.artbooktesting.repo.FakeArtRepositoryTest
+import com.bignerdranch.android.artbooktesting.viewmodel.ArtViewModel
+import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
 import javax.inject.Inject
 
 class ImageApiFragmentTest {
@@ -19,6 +34,33 @@ class ImageApiFragmentTest {
     @Before
     fun setup() {
         hiltRule.inject()
+    }
+
+    @Test
+    fun testSelectImage() {
+        val navController = Mockito.mock(NavController::class.java)
+        val selectedImageUrl = "atilsamancioglu.com"
+        val testViewModel = ArtViewModel(FakeArtRepositoryTest())
+
+        launchFragmentInHiltContainer<ImageApiFragment>(
+            factory = fragmentFactory,
+        ) {
+            Navigation.setViewNavController(requireView(),navController)
+            imageRecyclerAdapter.images = listOf(selectedImageUrl)
+            viewModel = testViewModel
+        }
+
+        Espresso.onView(ViewMatchers.withId(R.id.imageRecyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ImageRecyclerAdapter.ImageViewHolder>(
+                0,click()
+            )
+
+        )
+
+        Mockito.verify(navController).popBackStack()
+
+        assertThat(testViewModel.selectedImageUrl.getOrAwaitValue()).isEqualTo(selectedImageUrl)
+
     }
 
 }
