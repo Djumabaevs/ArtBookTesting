83201ab5b2bc3d736cf4977db0531071b81321ec
Djumabaevs <djumabaevb@gmail.com>
2021-07-08 10:16:14 +0600

    test navigation from details to image api

--- app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ArtDetailsFragmentTest.kt
+++ app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ArtDetailsFragmentTest.kt
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.artbooktesting.view
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.navigation.NavController
 import androidx.test.filters.MediumTest
+import com.bignerdranch.android.artbooktesting.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
 import javax.inject.Inject
 
 @MediumTest
@@ -23,7 +27,17 @@ class ArtDetailsFragmentTest {
 
     @Before
     fun setup() {
+        hiltRule.inject()
+    }
 
+    @Test
+    fun testNavigationFromArtDetailsToImageApi() {
+        val navController = Mockito.mock(NavController::class.java)
+        launchFragmentInHiltContainer<ArtDetailsFragment>(
+            factory = fragmentFactory
+        ) {
+
+        }
     }
 
 }
