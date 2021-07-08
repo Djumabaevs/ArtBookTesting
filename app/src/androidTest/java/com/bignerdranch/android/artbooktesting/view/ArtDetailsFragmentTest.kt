490bc5b5252b7567be8f5f20726ed06e66c5cfbb
Djumabaevs <djumabaevb@gmail.com>
2021-07-08 10:08:06 +0600

    inject hilt rule and fragment factory

--- app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ArtDetailsFragmentTest.kt
+++ app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ArtDetailsFragmentTest.kt
@@ -1,9 +1,12 @@
 package com.bignerdranch.android.artbooktesting.view
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.filters.MediumTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
+import javax.inject.Inject
 
 @MediumTest
 @HiltAndroidTest
@@ -12,6 +15,15 @@ class ArtDetailsFragmentTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    @Inject
+    lateinit var fragmentFactory: ArtFragmentFactory
+
+    @Before
+    fun setup() {
+
+    }
 
 }
