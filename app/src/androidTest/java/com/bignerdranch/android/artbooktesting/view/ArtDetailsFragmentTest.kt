78d8d467a4abf46bfcc291a5d7582148cd3ec68a
Djumabaevs <djumabaevb@gmail.com>
2021-07-08 10:04:49 +0600

    new art details test

---
+++ app/src/androidTest/java/com/bignerdranch/android/artbooktesting/view/ArtDetailsFragmentTest.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.artbooktesting.view
+
+import androidx.test.filters.MediumTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+
+@MediumTest
+@HiltAndroidTest
+class ArtDetailsFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+
+
+}
